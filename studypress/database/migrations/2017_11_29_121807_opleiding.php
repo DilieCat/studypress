77573b9420632cb16174bc7c15572d23f7cfaf63
Ivar van de Guchte <ivaryese123@gmail.com>
2017-11-29 13:31:27 +0100

    migrations aangemaakt

---
+++ studypress/database/migrations/2017_11_29_121807_opleiding.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Opleiding extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opleiding', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('crebo');
+            $table->integer('niveau');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opleiding');
+    }
+}
