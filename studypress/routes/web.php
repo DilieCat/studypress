0c84a03f8c255dc265e2298e3977344d3d928eb8
Dilie <dilie_90@hotmail.com>
2017-12-01 09:48:54 +0100

    User kan inloggen

--- studypress/routes/web.php
+++ studypress/routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index');
+
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
