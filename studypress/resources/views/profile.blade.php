b8525c82356cb172f0735a19317e2d39b6b0b9ce
Dilie <dilie_90@hotmail.com>
2017-12-12 11:40:25 +0100

    Profiel pagina werkt, layout staat nog open voor aanpassing

--- studypress/resources/views/profile.blade.php
+++ studypress/resources/views/profile.blade.php
@@ -3,10 +3,17 @@
 @section('title', 'Profiel')
 
 @section('content')
+	<span class="ov-nummer">Ov-nummer: {{ Auth::user()->ov_nummer }}</span>
 <div class="profile">
-	<h3 class="dilie-cat">Student Gegevens</h3>
-Naam: {{ Auth::user()->name }} <br>
-Email: {{ Auth::user()->email }} <br>
-Woonplaats: {{ Auth::user()->woonplaats }}
+
+	<h3>Student Gegevens</h3>
+<b>Naam:</b> {{ Auth::user()->name }} <br>
+<b>Email:</b> {{ Auth::user()->email }} <br>
+<b>Woonplaats:</b> {{ Auth::user()->woonplaats }} <br>
+<b>Mobiele nummer:</b> {{ Auth::user()->telnummer }} <br>
+<b>Cohort:</b> {{ Auth::user()->cohort }} <br>
+<b>Klas:</b> {{  $klas[0]->name }} <br>
+
+
 </div>
 @endsection 
